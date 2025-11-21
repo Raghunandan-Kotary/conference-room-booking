58e3da8f3e1102b524187ba1937c2c447166f13b
Raghunandan-Kotary <kotaryraghunandan@gmail.com>
2025-11-21 10:02:58 +0530

    Refactor pro.h to add new functions and increase capacity

--- pro.h
+++ pro.h
@@ -1,3 +1,4 @@
+
 // pro.h
 #ifndef BOOKING_H
 #define BOOKING_H
@@ -19,12 +20,12 @@ typedef struct Booking {
 
 typedef struct QueueNode {
     char name[50];
-    int roomNo; // Stores which room they are waiting for
+    int roomNo;
     struct QueueNode* next;
 } QueueNode;
 
 // ---------- HEAP ----------
-#define MAX_ROOMS 50
+#define MAX_ROOMS 200
 typedef struct {
     Room* arr[MAX_ROOMS];
     int size;
@@ -32,22 +33,25 @@ typedef struct {
 
 // ---------- FUNCTION DECLARATIONS ----------
 
-// BST (Rooms)
+// BST
 Room* createRoom(int no, int cap);
 Room* insertRoom(Room* root, int no, int cap);
 void inorder(Room* root);
 Room* searchRoom(Room* root, int no);
+Room* deleteRoom(Room* root, int roomNo);
 
 // Linked List (Bookings)
 void addBooking(char name[], int roomNo, int start, int end);
 void displayBookings();
-int removeFromBookings(char *name); 
+int removeFromBookings(char *name);
+void removeRoomRelatedBookings(int roomNo);
 
 // Queue (Waiting List)
-void enqueue(char name[], int roomNo); // Takes roomNo
+void enqueue(char name[], int roomNo);
 void dequeue();
 void showQueue();
 int removeFromWaitingList(char *name);
+void removeWaitingListForRoom(int roomNo);
 
 // Booking Operations
 void bookRoom(Room* root, int no, char user[], int start, int end);
@@ -56,14 +60,18 @@ void cancelBooking(Room* root, char *name);
 // Conflict Detection
 int hasConflict(int roomNo, int start, int end);
 
-// Min Heap Functions
+// Heap
 void initHeap(MinHeap* heap);
 void insertHeap(MinHeap* heap, Room* r);
 Room* extractMin(MinHeap* heap);
 Room* autoAllocate(MinHeap* heap, int neededCap);
 Room* findRoomToWaitFor(MinHeap* heap, int neededCap);
+void rebuildHeap(MinHeap* heap, Room* root);
 
-// Helper Functions
-void clearInputBuffer(); 
+// Helper
+void clearInputBuffer();
+
+// NEW: Auto-generate rooms 101–200
+void autoGenerateRooms(Room **root, MinHeap *heap);
 
 #endif
